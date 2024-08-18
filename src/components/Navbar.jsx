50ca0d18ed1a504adfebf436a1ea0b715c6885d1
Sam Sharma <samsharma.dev@gmail.com>
2024-08-18 14:57:13 -0700

    set up menu for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { usePrivy } from "@privy-io/react-auth";
 import CustomButton from "./CustomButton";
 import { useNavigate } from "react-router-dom";
 import { RiMentalHealthFill } from "react-icons/ri";
+import { navlinks } from "../constants";
 const Navbar = () => {
   const { ready, authenticated, login, user, logout } = usePrivy();
-
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const [isActive, setIsActive] = useState("Dashboard");
   const navigate = useNavigate();
 
   const handleLoginLogout = useCallback(() => {
@@ -19,8 +21,12 @@ const Navbar = () => {
 
   return (
     <div className="mb-[35px] flex w-full flex-col-reverse justify-between gap-6 sm:flex-row md:flex-row">
-      <div className="cursor-pointer" onClick={() => navigate("/")}>
+      <div
+        className="hidden cursor-pointer gap-4 sm:flex sm:flex-row sm:items-center"
+        onClick={() => navigate("/")}
+      >
         <RiMentalHealthFill style={{ color: "#2563eb", fontSize: "50px" }} />
+        <p className="navbar-txt blue_gradient text-center">SteadyPathAI</p>
       </div>
       <div className="hidden flex-row justify-end gap-2 sm:flex">
         <CustomButton
@@ -29,6 +35,63 @@ const Navbar = () => {
           styles={authenticated ? "bg-[#1dc071]" : "bg-[#2563eb]"}
           handleClick={handleLoginLogout}
         />
+      </div>
+      <div className="relative flex items-center justify-between sm:hidden">
+        <div
+          className="flex h-[40px] cursor-pointer items-center justify-center sm:hidden"
+          onClick={() => navigate("/")}
+        >
+          <RiMentalHealthFill style={{ color: "#2563eb", fontSize: "40px" }} />
+        </div>
+        <img
+          src="src/assets/icons/menu.svg"
+          alt="Menu icon"
+          className="h-[34px] w-[34px] cursor-pointer object-contain hover:grayscale"
+          onClick={() => {
+            setToggleDrawer((prev) => !prev);
+          }}
+        />
+
+        <div
+          className={`shadow-secondary absolute left-0 right-0 top-[60px] z-10 bg-[#1c1c24] py-4 ${
+            !toggleDrawer ? "-translate-y-[100vh]" : "translate-y-0"
+          } transition-all duration-700`}
+        >
+          <ul className="mb-4">
+            {navlinks.map((link) => (
+              <li
+                key={link.name}
+                className={`flex items-center p-4 ${isActive === link.name && "bg-[#3a3a43]"}`}
+                onClick={() => {
+                  setIsActive(link.name);
+                  setToggleDrawer(false);
+                  navigate(link.link);
+                }}
+              >
+                <img
+                  src={link.imgUrl}
+                  alt={link.name}
+                  className={`h-[24px] w-[24px] object-contain ${
+                    isActive === link.name ? "grayscale-0" : "grayscale"
+                  }`}
+                />
+                <p
+                  className={`font-epilogue ml-[20px] text-[14px] font-semibold ${
+                    isActive === link.name ? "text-[#0891b2]" : "text-[#808191]"
+                  }`}
+                >
+                  {link.name}
+                </p>
+              </li>
+            ))}
+            <li className="flex items-center p-4">
+              <img
+                src="src/assets/icons/sun.svg"
+                className={`h-[24px] w-[24px] object-contain`}
+              />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,9 +4,9 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div
-    className={`h-[48px] w-[48px] rounded-[10px] xl:w-[192px] xl:gap-4 ${
+    className={`h-[48px] w-[48px] rounded-[10px] xl:w-[216px] xl:gap-4 ${
       isActive && isActive === name && "bg-[#2c2f32]"
-    } flex items-center justify-center xl:justify-start ${
+    } flex items-center justify-center xl:justify-start xl:px-4 ${
       !disabled && "cursor-pointer"
     } ${styles}`}
     onClick={handleClick}
@@ -26,9 +26,9 @@ const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
 
 const Sidebar = () => {
   const navigate = useNavigate();
-  const [isActive, setIsActive] = useState("dashboard");
+  const [isActive, setIsActive] = useState("Dashboard");
   return (
-    <div className="mt-8 flex w-[76px] flex-1 flex-col items-center justify-between gap-48 rounded-[20px] bg-[#1c1c24] py-4 xl:w-[240px] xl:text-left">
+    <div className="mt-8 hidden w-[76px] flex-1 flex-col items-center justify-between gap-48 rounded-[20px] bg-[#1c1c24] py-4 sm:flex xl:w-[240px] xl:text-left">
       <div className="flex flex-col items-center justify-center gap-3">
         {navlinks.map((link) => (
           <div>
