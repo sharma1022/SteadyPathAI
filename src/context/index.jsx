a51522cc0c2ded038e91cd619ee35ed385b12966
Sam Sharma <samsharma.dev@gmail.com>
2024-08-19 11:55:32 -0700

    Create db functions

---
+++ src/context/index.jsx
@@ -0,0 +1,116 @@
+import React, { createContext, useContext, useState, useCallback } from "react";
+import { db } from "../utils/dbConfig";
+import { Users, Records } from "../utils/schema";
+
+import { eq } from "drizzle-orm";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [records, setRecords] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const getUsers = useCallback(async () => {
+    try {
+      const result = await db.select().from(Users).execute();
+      setUsers(result);
+    } catch (error) {
+      console.error("Error fetching users.", error);
+    }
+  }, []);
+
+  const getUserByEmail = useCallback(async (email) => {
+    try {
+      const result = await db
+        .select()
+        .from(Users)
+        .where(eq(Users.createdBy, email));
+
+      if (result.length > 0) {
+        setCurrentUser(result[0]);
+      }
+    } catch (error) {
+      console.error("Error fetching user by email.", error);
+    }
+  }, []);
+
+  const createUser = useCallback(async (userData) => {
+    try {
+      const newUser = await db
+        .insert(Users)
+        .values(userData)
+        .returning()
+        .execute();
+
+      setUsers((prevUsers) => [...prevUsers, newUser[0]]);
+    } catch (error) {
+      console.error("Error creating user.", error);
+      return null;
+    }
+  }, []);
+
+  const getUserRecords = useCallback(async (email) => {
+    try {
+      const result = await db
+        .select()
+        .from(Records)
+        .where(eq(Records.createdBy, email))
+        .execute();
+      setRecords(result);
+    } catch (error) {
+      console.error("Error fetching user records:", error);
+    }
+  }, []);
+
+  const createRecord = useCallback(async (recordData) => {
+    try {
+      const newRecord = await db
+        .insert(Records)
+        .values(recordData)
+        .returning({ id: Records.id })
+        .execute();
+      setRecords((prevRecords) => [...prevRecords, newRecord[0]]);
+      return newRecord[0];
+    } catch (error) {
+      console.error("Error creating record:", error);
+      return null;
+    }
+  }, []);
+
+  const updateRecord = useCallback(async (recordData) => {
+    try {
+      const { documentID, ...dataToUpdate } = recordData;
+      console.log(documentID, dataToUpdate);
+      const updatedRecords = await db
+        .update(Records)
+        .set(dataToUpdate)
+        .where(eq(Records.id, documentID))
+        .returning();
+    } catch (error) {
+      console.error("Error updating record:", error);
+      return null;
+    }
+  }, []);
+
+  return (
+    <StateContext.Provider
+      value={{
+        users,
+        records,
+        getUsers,
+        getUserByEmail,
+        createUser,
+        getUserRecords,
+        createRecord,
+        currentUser,
+        updateRecord,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+// Custom hook to use the context
+export const useStateContext = () => useContext(StateContext);
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import App from "./App";
 
 import "./index.css";
 import { PrivyProvider } from "@privy-io/react-auth";
+import { StateContextProvider } from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -21,7 +22,9 @@ root.render(
     }}
   >
     <Router>
-      <App />
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </Router>
   </PrivyProvider>,
 );
