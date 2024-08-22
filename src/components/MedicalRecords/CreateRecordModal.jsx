e0dae65d8eb4d91ce193761b89ac457eb790c452
Sam Sharma <samsharma.dev@gmail.com>
2024-08-21 18:37:05 -0700

    create Modal and create folder working

---
+++ src/components/MedicalRecords/CreateRecordModal.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Modal from "../../components/Modal";
+const CreateRecordModal = ({ isOpen, onClose, onCreate }) => {
+  const [folderName, setFolderName] = useState("");
+
+  const handleCreate = () => {
+    onCreate(folderName);
+    setFolderName("");
+  };
+  return (
+    <Modal
+      title="Create Record"
+      isOpen={isOpen}
+      onClose={onClose}
+      onAction={handleCreate}
+      actionLabel="Create folder"
+    >
+      <div className="grid gap-y-4">
+        <div>
+          <label
+            htmlFor="folder-name"
+            className="mb-2 block text-sm text-white"
+          >
+            Record Name
+          </label>
+          <div className="relative">
+            <input
+              type="text"
+              value={folderName}
+              onChange={(e) => setFolderName(e.target.value)}
+              required
+              className="block w-full rounded-lg border-2 px-4 py-3 text-sm focus:border-2 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-400 dark:placeholder-neutral-500"
+            />
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default CreateRecordModal;
--- src/pages/MedicalRecords/MedicalRecords.jsx
+++ src/pages/MedicalRecords/MedicalRecords.jsx
@@ -2,23 +2,96 @@ import React, { useState, useEffect } from "react";
 import { Sidebar } from "../../components";
 import { FiPlusCircle } from "react-icons/fi";
 import RecordCard from "../../components/MedicalRecords/RecordCard";
+import { useNavigate } from "react-router-dom";
+import { usePrivy } from "@privy-io/react-auth";
+import { useStateContext } from "../../context";
+import CreateRecordModal from "../../components/MedicalRecords/CreateRecordModal";
 const MedicalRecords = () => {
-  const handleOpenModal = () => {};
+  const navigate = useNavigate();
+  const { user } = usePrivy();
+  const { records, getUserRecords, createRecord, getUserByEmail, currentUser } =
+    useStateContext();
+  const [userRecords, setUserRecords] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      getUserByEmail(user.email.address);
+      getUserRecords(user.email.address);
+    }
+  }, [user, getUserByEmail, getUserRecords]);
+
+  useEffect(() => {
+    setUserRecords(records);
+    localStorage.setItem("userRecords", JSON.stringify(records));
+  }, [records]);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const createFolder = async (foldername) => {
+    try {
+      if (currentUser) {
+        const newRecord = await createRecord({
+          userId: currentUser.id,
+          recordName: foldername,
+          analysisResult: "",
+          kanbanRecords: "",
+          createdBy: user.email.address,
+        });
+
+        if (newRecord) {
+          getUserRecords(user.email.address);
+          handleCloseModal();
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      handleCloseModal();
+    }
+  };
+
+  const handleNavigate = (name) => {
+    const filteredRecords = userRecords.filter(
+      (record) => record.recordName === name,
+    );
+    navigate(`/medical-records/${name}`, {
+      state: filteredRecords[0],
+    });
+  };
+
   return (
     <div className="flex flex-row items-start gap-6">
       <Sidebar />
       <div className="flex flex-wrap gap-[26px]">
         <button
           type="button"
-          className="txt-sm mt-6 inline-flex items-center gap-x-2 rounded-full border border-neutral-700 bg-[#13131a] px-4 py-2 font-medium text-white shadow-sm hover:bg-neutral-800"
-          // onClick={handleOpenModal}
+          className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-[#13131a] dark:text-white dark:hover:bg-neutral-800"
+          onClick={handleOpenModal}
         >
-          <FiPlusCircle size={28} />
+          <FiPlusCircle />
           Create Record
         </button>
 
-        <div className="grid w-full sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
-          <RecordCard />
+        <CreateRecordModal
+          isOpen={isModalOpen}
+          onClose={handleCloseModal}
+          onCreate={createFolder}
+        />
+
+        <div className="grid w-full gap-4 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
+          {userRecords?.map((record) => (
+            <RecordCard
+              key={record.recordName}
+              record={record}
+              onNavigate={handleNavigate}
+            />
+          ))}
         </div>
       </div>
     </div>
